48a1248631777e94d406c8615cc3cec5266676c0
320028626 <vinay.r@philips.com>
2020-12-15 20:42:17 +0530

    Modified the code to search with keyword, country and display the latest news.

--- src/app/news-home/components/latest-news/latest-news.component.ts
+++ src/app/news-home/components/latest-news/latest-news.component.ts
@@ -1,4 +1,7 @@
+// Angular imports
 import { Component, OnInit } from '@angular/core';
+
+// Custom imports
 import { LoggingService } from 'src/app/shared';
 import { NewsResponse } from '../../interfaces/news-response';
 import { NewsGetterService } from '../../services/news-getter.service';
@@ -10,6 +13,7 @@ import { NewsGetterService } from '../../services/news-getter.service';
 export class LatestNewsComponent implements OnInit {
     public newsResponse: NewsResponse[] = [];
     public isLoading = false;
+    public country = '';
     constructor(
         private newsGetterService: NewsGetterService,
         private log: LoggingService,
@@ -22,12 +26,26 @@ export class LatestNewsComponent implements OnInit {
     /**
      * Gets the latest news
      */
-    private getLatestNews() {
+    public getLatestNews(keyword?: string) {
         this.isLoading = true;
-        this.newsGetterService.getLatestNews().subscribe((response: NewsResponse[]) => {
+        this.newsGetterService.getLatestNews(this.country, keyword).subscribe((response: NewsResponse[]) => {
             this.newsResponse = response;
             this.isLoading = false;
             this.log.info('We got the latest news.');
-        });
+        },
+            (error) => {
+                this.log.error(JSON.stringify(error));
+                this.isLoading = false;
+            });
     }
+
+    /**
+     * Updates the country as given country and fetches the news for it.
+     * @param country country to fetch the news for.
+     */
+    public fetchLatestNewsForCountry(country: string) {
+        this.country = country;
+        this.getLatestNews();
+    }
+
 }
